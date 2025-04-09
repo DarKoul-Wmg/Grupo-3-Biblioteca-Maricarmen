a74bf05c887fd74bf48d1802c4d883141dcd1af6
Ivan D.S. <ivandiazsanchez1996@gmail.com>
2025-04-09 18:46:06 +0200

    Changes to api to be able to edit certain date from users and upload images

--- biblioteca-maricarmen/settings.py
+++ biblioteca-maricarmen/settings.py
@@ -119,8 +119,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = 'biblioteca/static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = 'static/'
+
+# Define the path for storing uploaded media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # This will create a 'media' folder in your project directory
+# URL to access media files
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
