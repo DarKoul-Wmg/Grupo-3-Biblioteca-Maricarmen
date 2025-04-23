f68164adafd20b218c034fdde2f760c4cceff654
William Molina <williammg0904@gmail.com>
2025-04-24 00:57:44 +0200

    Fix: In edit Llibre (admin panel), now can see default values in the fields Autor and Editorial

--- biblioteca/forms.py
+++ biblioteca/forms.py
@@ -4,14 +4,14 @@ from .models import Llibre
 
 class LlibreForm(forms.ModelForm):
     autor = forms.CharField(
-        widget=autocomplete.ListSelect2(
+        widget=autocomplete.Select2(
             url='autor-autocomplete'
         ),
         required=False,
         label="Autor"
     )
     editorial = forms.CharField(
-        widget=autocomplete.ListSelect2(
+        widget=autocomplete.Select2(
             url='editorial-autocomplete'
         ),
         required=False,
@@ -21,3 +21,15 @@ class LlibreForm(forms.ModelForm):
     class Meta:
         model = Llibre
         fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Si el formulario está vinculado a una instancia, cargamos los valores iniciales
+        if self.instance and self.instance.pk:
+            # Configurar el valor inicial para el widget de autocompletar
+            if self.instance.autor:
+                self.fields['autor'].widget.choices = [(self.instance.autor, self.instance.autor)]
+                self.fields['autor'].initial = self.instance.autor
+            if self.instance.editorial:
+                self.fields['editorial'].widget.choices = [(self.instance.editorial, self.instance.editorial)]
+                self.fields['editorial'].initial = self.instance.editorial
